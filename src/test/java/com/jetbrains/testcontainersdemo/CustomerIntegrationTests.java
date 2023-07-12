326f96c9a11d648e4987725aa03f2b1fe6cdd51f
Julia J-B <4juliko@gmail.com>
2023-07-11 20:15:18 -0700

    changes added

--- src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
+++ src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
@@ -1,22 +1,37 @@
 package com.jetbrains.testcontainersdemo;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
 public class CustomerIntegrationTests {
 
     @Autowired
     private CustomerDao customerDao;
 
+
+    private static MySQLContainer container = (MySQLContainer) new MySQLContainer("mysql:8.0.33")
+            .withReuse(true);
+
     @Test
     void when_using_a_clean_db_this_should_be_empty() {
         List<Customer> customers = customerDao.findAll();
         assertThat(customers).hasSize(2);
+        System.out.println(customers);
+    }
+
+    @BeforeAll
+    public static void setup(){
+        container.start();
     }
 }
